90de89384bf3cd9af13d17e3a8917678fe970685
Tv-bd2 <rspremium64@gmail.com>
2026-06-10 11:44:59 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
 document.addEventListener('DOMContentLoaded', function() {
+
     const container = document.getElementById('channel-container');
 
+
+
     // JSON ফাইল থেকে চ্যানেল লোড করা
+
     fetch('playlist.json')
+
         .then(response => response.json())
+
         .then(data => {
+
             data.forEach(channel => {
+
                 const li = document.createElement('li');
+
                 li.innerHTML = `
+
                     <a href="javascript:void(0);" onclick="player.location.href='${channel.url}'">
+
                         <img src="${channel.image}" alt="${channel.name}">
+
                     </a>
+
                 `;
+
                 container.appendChild(li);
+
             });
+
         })
+
         .catch(error => {
+
             console.error('Error loading playlist:', error);
+
             container.innerHTML = '<p style="color:red; font-size:10px;">Error!</p>';
+
         });
+
 });
 
+
+
 // রাইট ক্লিক বন্ধ করার ফাংশন (ঐচ্ছিক)
+
 function disableClick() {
+
     document.oncontextmenu = function() { return false; };
+
 }
